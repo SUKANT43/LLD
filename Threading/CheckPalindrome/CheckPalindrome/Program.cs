1884c9c838008fb2d1beec5c04b3eb0ed2c9260f
SUKANT43 <sukant98657@gmail.com>
2026-05-27 18:44:03 +0530

    Check palindrome 3rd way added

--- Threading/CheckPalindrome/CheckPalindrome/Program.cs
+++ Threading/CheckPalindrome/CheckPalindrome/Program.cs
@@ -15,14 +15,78 @@ namespace CheckPalindrome
 
             string[] words = word.Split(' ');
 
-            Parallel.ForEach(words, wor =>
-            {
-                Console.WriteLine(wor+"="+IsPalindrome(wor));
-            });
+            object lockObj = new object();
 
+            //1
+            //Parallel.ForEach(words, wor =>
+            //{
+            //        Console.WriteLine(wor + "=" + IsPalindrome(wor));
+            //});
+
+
+            //2
+            //List<Thread> threads = new List<Thread>();
+
+            //foreach (var wor in words)
+            //{
+            //    Thread thread = new Thread(() =>
+            //    {
+            //        Console.WriteLine(wor + "=" + IsPalindrome(wor));
+            //    });
+
+            //    threads.Add(thread);
+            //    thread.Start();
+
+            //    //thread.Join();
+            //}
+
+            //foreach(Thread thread in threads)
+            //{
+            //    thread.Join();
+            //}
+
+            //3
+
+            int threadCount = 3;
+
+            List<Thread> threads = new List<Thread>();
+
+            int currentIndex = 0;
+            for (int i = 0; i < threadCount; i++)
+            {
+                Thread thread = new Thread(() =>
+                  {
+                      while (true)
+                      {
+                          string currentWord;
+                          lock (lockObj)
+                          {
+                              if (currentIndex >= words.Length)
+                                  return;
+
+                              currentWord = words[currentIndex++];
+                          }
+
+                          Console.WriteLine(
+                              $"Thread {Thread.CurrentThread.ManagedThreadId}: " + $"{currentWord} = {IsPalindrome(currentWord)}");
+
+                          Thread.Sleep(100);
+
+                      }
+                  });
+
+                threads.Add(thread);
+                thread.Start();
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
 
 
             Console.ReadLine();
+
         }
 
         public static bool IsPalindrome(string word)
@@ -41,3 +105,4 @@ namespace CheckPalindrome
 
     }
 }
+
