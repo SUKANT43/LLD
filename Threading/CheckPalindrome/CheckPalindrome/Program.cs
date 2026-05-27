206c6cecc97b22b842e6891c8d10a479655e5966
SUKANT43 <sukant98657@gmail.com>
2026-05-27 18:03:13 +0530

    check palindrome

---
+++ Threading/CheckPalindrome/CheckPalindrome/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CheckPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "heh hih lwk mam wow huh hell";
+
+            string[] words = word.Split(' ');
+
+            Parallel.ForEach(words, wor =>
+            {
+                Console.WriteLine(wor+"="+IsPalindrome(wor));
+            });
+
+
+
+            Console.ReadLine();
+        }
+
+        public static bool IsPalindrome(string word)
+        {
+
+            int i = 0;
+            int j = word.Length - 1;
+
+            while (i <= j)
+            {
+                if (word[i++] != word[j--]) return false;
+            }
+
+            return true;
+        }
+
+    }
+}
